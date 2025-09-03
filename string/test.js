ac87e8d5248240b391e4bf83a493efc5bc1a707a
Ankur Maurya <102471702+ankurm20@users.noreply.github.com>
2025-09-03 14:03:53 +0000

    String code file

---
+++ string/test.js
@@ -0,0 +1,21 @@
+let str1 = "Ankur"
+let str2 = " Maurya"
+let str3 = str1 + str2
+
+// console.log(str3)
+// console.log("1" + 2)
+// console.log(1 + "3")
+// console.log(1 + 2 + "5")
+// console.log("1" + 2 + 3)
+
+// // bool
+// console.log(true)
+// console.log(false)
+
+
+let x = 100
+let y = 200
+++x
+y++
+console.log(x)
+console.log(y)
