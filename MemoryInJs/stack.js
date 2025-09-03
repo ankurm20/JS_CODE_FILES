4505b2ab5fb449c8dc6d21bac3be091fb08c3ce3
Ankur Maurya <102471702+ankurm20@users.noreply.github.com>
2025-09-03 14:36:15 +0000

    string

---
+++ MemoryInJs/stack.js
@@ -0,0 +1,19 @@
+//stack (primitive) , heap (non-primitive)
+let myYoutubename = "ankur20"
+let anothername = myYoutubename
+anothername = "maurya201"
+
+console.log(myYoutubename)
+console.log(anothername)
+
+let userOne = {
+    userName : "ankur",
+    userEmail : "an@gmail.com"
+}
+console.table(userOne)
+
+let userTwo = {
+    userName : "shubh",
+    userEmail : "an@gmail.com"
+}
+console.table(userTwo)
---
+++ string/str.js
@@ -0,0 +1,14 @@
+const name = "am "
+const other = "I am Fine "
+
+// console.log(`Hello is this ${name} how are you. ${other}`);
+
+
+const gameName = new String('amaurya')
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf('r'))
