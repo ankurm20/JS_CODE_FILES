f7a65e0eca992f54de5069a0e42c81f2eba5bedf
Ankur Maurya <102471702+ankurm20@users.noreply.github.com>
2025-09-04 05:03:24 +0000

    math code file

---
+++ math/01math.js
@@ -0,0 +1,20 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(3,5,8,2,6,9));
+
+
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+console.log((Math.random() * 10) + 1)
+
+
+const min = 10
+const max = 20
+
+Math.random()
+console.log(Math.floor(Math.random() * 10) + 1)
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
